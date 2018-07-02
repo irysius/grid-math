717e073da1f63e13f171accbd875f9b670625898
irysius <lestersy@hotmail.com>
2018-07-02 19:34:47 -0400

    Adding index.ts back. Need to rethink how to npm publish

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+// This is the primary endpoint for usage with nodejs.
+import * as _Iterable from './helpers/Iterable';
+export let helpers = {
+    Iterable: _Iterable
+};
+
+import * as _PathExecutor from './pathfinding/PathExecutor';
+import * as _tools from './pathfinding/tools';
+export let pathfinding = {
+    PathExecutor: _PathExecutor,
+    tools: _tools
+};
+
+import * as _Cell from './Cell';
+import * as _CellCoord from './CellCoord';
+import * as _CoordManager from './CoordManager';
+import * as _Direction from './Direction';
+import * as _Gravity from './Gravity';
+import * as _GridPosition from './GridPosition';
+import * as _Rect from './Rect';
+import * as _ScreenPosition from './ScreenPosition';
+import * as _ScreenRect from './ScreenRect';
+import * as _Size from './Size';
+import * as _Vector2 from './Vector2';
+import * as _WorldPosition from './WorldPosition';
+import * as _WorldRect from './WorldRect';
+
+export let Cell = _Cell;
+export let CellCoord = _CellCoord;
+export let CoordManager = _CoordManager;
+export let Direction = _Direction;
+export let Gravity = _Gravity;
+export let GridPosition = _GridPosition;
+export let Rect = _Rect;
+export let ScreenPosition = _ScreenPosition;
+export let ScreenRect = _ScreenRect;
+export let Size = _Size;
+export let Vector2 = _Vector2;
+export let WorldPosition = _WorldPosition;
+export let WorldRect = _WorldRect;
